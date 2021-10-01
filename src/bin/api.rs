ed20881d9621b4cc3462924e38b5de3d7fece92a
Fabien Bellanger <fabien.bellanger@apitic.com>
2021-10-01 12:34:52 +0200

    Init Axum project

---
+++ src/bin/api.rs
@@ -0,0 +1,13 @@
+use axum::{handler::get, Router};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
