a663bd0bed3b877c51dba54372d3b5b1e4b72546
Fabien Bellanger <fabien.bellanger@apitic.com>
2022-04-20 18:47:46 +0200

    Logger again

--- src/logger.rs
+++ src/logger.rs
@@ -5,6 +5,7 @@ use tracing_subscriber::{prelude::*, EnvFilter, Registry};
 /// Register a subscriber as global default to process span data.
 ///
 /// It should only be called once!
+// Read: https://docs.rs/tracing-subscriber/latest/tracing_subscriber/layer/trait.Layer.html
 pub fn init(environment: &str, path: &str, filename: &str) {
     // Production environment
     let prod_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("eeror"));
@@ -14,11 +15,11 @@ pub fn init(environment: &str, path: &str, filename: &str) {
         .with_thread_ids(false) // include the thread ID of the current thread
         .with_thread_names(false) // include the name of the current thread
         .with_file(true)
+        .with_line_number(true)
         .json();
     let file_appender = tracing_appender::rolling::daily(path, filename);
     let prod_layer = tracing_subscriber::fmt::layer()
         .event_format(prod_format)
-        .with_line_number(true)
         .with_writer(file_appender);
     let prod_subscriber = Registry::default().with(prod_filter).with(prod_layer);
 
@@ -30,10 +31,10 @@ pub fn init(environment: &str, path: &str, filename: &str) {
         .with_thread_ids(false) // include the thread ID of the current thread
         .with_thread_names(false) // include the name of the current thread
         .with_file(true)
+        .with_line_number(true)
         .compact();
     let dev_layer = tracing_subscriber::fmt::layer()
         .event_format(dev_format)
-        .with_line_number(true)
         .with_writer(std::io::stdout);
     let dev_subscriber = Registry::default().with(dev_filter).with(dev_layer);
 
